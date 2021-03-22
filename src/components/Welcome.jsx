709460f496e7fafb379239cde50445f20185b1f5
pwambui93@gmail.com <pwambui93@gmail.com>
2021-03-22 13:20:25 +0300

    Setup Hotel container

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            <h1>welcome component</h1>
+           <div className= 'welcome-page'>
+             <div className="welcome-info">
+             <h1 className= 'text-center'>Book the Best Hotel At Affordable prices</h1>
+             <button> view hotels <i class="fa fa-home" aria-hidden="true"></i></button>
+             </div>
+            
+           </div>
         </div>
     );
 }
---
+++ src/containers/Hotel.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Hotel extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+Hotel.propTypes = {
+
+};
+
+export default Hotel;
