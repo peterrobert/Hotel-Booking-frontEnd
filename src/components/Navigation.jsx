ceef8122214db3adcce1a9a0b9ec0f7674b89e21
pwambui93@gmail.com <pwambui93@gmail.com>
2021-03-23 08:13:49 +0300

    Create navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            
+            
+        </div>
+    );
+}
+
+export default Navigation;
--- src/containers/Hotel.jsx
+++ src/containers/Hotel.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Navigation from '../components/Navigation';
 import { dataHotel } from '../redux/Actions/hotelAction';
 
 class Hotel extends Component {
@@ -16,7 +17,15 @@ class Hotel extends Component {
        const {data, status} = this.props.resturants.hotels.data
        console.log(data, status)
         return (
-            <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-3">
+                    <Navigation />
+                    </div>
+                    <div className="col-md-9">
+                        
+                    </div>
+                </div>
 
             </div>
         );
